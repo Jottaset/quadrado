b4181e7ee69bc8e1d16741622c50be0f1b63d73e
Jottaset <aluno@MacBook-Pro-de-Aluno.local>
2018-10-29 17:58:17 -0400

    Commit Pjt Quadrado

---
+++ Quadrado/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Quadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Quadrado quadrado = new Quadrado();
+
+            Console.WriteLine("Digite um valor para para o lado 1: ");
+            //quadrado.defineValorLado1 (Int32.Parse(Console.ReadLine()));
+
+            /*Console.WriteLine("Digite um valor para para o lado 2: ");
+            quadrado.defineValorLado2 (Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite um valor para para o lado 3: ");
+            quadrado.defineValorLado3 (Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite um valor para para o lado 4: ");
+            quadrado.defineValorLado4 (Int32.Parse(Console.ReadLine()));*/
+
+            string texto;
+            int valor = Int32.Parse(Console.ReadLine());
+
+            if (valor >= 5){
+                quadrado.defineValorLado1(valor);
+                texto = "Deu certo e o valor definido foi: " + quadrado.getValor1();
+            }
+            else{
+                texto = "Nao deu certo e o valor foi: " + quadrado.getValor1();
+            }
+
+            Console.WriteLine(texto);
+
+        }
+    }
+}
---
+++ Quadrado/Quadrado.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Quadrado
+{
+    public class Quadrado
+    {
+        private int lado1;
+        private int lado2;
+        private int lado3;
+        private int lado4;
+
+        public void defineValorLado1(int ValorLado1){
+            lado1 = ValorLado1;
+        }
+
+        public int getValor1(){
+            return lado1;
+        }
+
+        public void defineValorLado2(int ValorLado2)
+        {
+            lado2 = ValorLado2;
+        }
+
+        public int getValor2()
+        {
+            return lado2;
+        }
+
+        public void defineValorLado3(int ValorLado3)
+        {
+            lado3 = ValorLado3;
+        }
+
+        public int getValor3()
+        {
+            return lado3;
+        }
+
+        public void defineValorLado4(int ValorLado4)
+        {
+            lado4 = ValorLado4;
+        }
+
+        public int getValor4()
+        {
+            return lado4;
+        }
+    }
+
+}
